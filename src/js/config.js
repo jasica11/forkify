2b2734adf764e1eca8c04fd68c6af2e8ca11919d
jasica11 <jasicah11@gmail.com>
2023-06-15 08:20:42 +0600

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+//constant and reuseable varibales are getherd in this file
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+
+export const MODAL_CLOSE_SEC = 2.5;
+
+export const KEY = 'd7c0d70f-2162-46ab-8548-1e327788e18c';
